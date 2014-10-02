212d6de2742d5d570f475c14acbc62fbd103369f
Diego Medina <d.medina1290@gmail.com>
2014-10-02 10:48:14 -0400

    Added initial controllers, models, and deleted turbolinks

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141002135204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "chapters", force: true do |t|
+    t.string   "name"
+    t.text     "body"
+    t.integer  "order"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
